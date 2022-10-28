3704631795b136ed4e745f0b01aa52620fa45659
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-10-28 04:53:41 -0700

    Complete src/App.css

---
+++ src/Mainpage.jsx
@@ -0,0 +1,35 @@
+
+function Mainpage(){
+return <div className="main-container">
+<div className="canva">
+
+<p className="canvasp"> &#x2022;&#x2022;&#x2022;</p>
+</div>
+<div> <img className="profile-image" src="https://media-exp1.licdn.com/dms/image/C5603AQFoBDRgg_q2Ew/profile-displayphoto-shrink_200_200/0/1529882412560?e=1672272000&v=beta&t=bwxwH01RgiZfNQ6NhNzhoo9bRuHCU4tsGB9DOyM12Mw"/></div>
+<div>
+    <h1> Ogodo Onyebuchi</h1>
+</div>
+<div className="all-links">
+<a className="links" href=""> Twitter Link</a>
+<a className="links" href=""> Zuri Team</a>
+<a className="links" href=""> Zuri Books</a>
+<a className="links" href=""> Python Books</a>
+<a className="links" href=""> Background Check for Coders</a>
+<a className="links" href=""> Design Books</a>
+</div>
+<div className="slack-git"> 
+<img className="slack-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Slack_icon_2019.svg/2048px-Slack_icon_2019.svg.png"/>
+<img className="github-img" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>
+</div>
+<hr />
+<footer>
+<div className="flex-container">
+    <div id="Zuri"> <p> Zuri<span id="dot">&#x2022;</span>Internship</p> </div>
+    <div id="hng"> <p id="hngp"> HNG Intership 9 Frontend Task</p></div>
+    <div><img id="ingressive" src="https://i0.wp.com/ingressive.org/wp-content/uploads/2020/08/I4G-Logo-Main.png?fit=2225%2C539&ssl=1"/> </div>
+</div>
+
+</footer>
+</div>
+}
+export default Mainpage;
